8ea720058372b2cd6db9162cdf9d411650c44868
James Pharis <jamesmpharis@gmail.com>
2024-03-15 21:23:20 -0400

    Added Error Handling

--- client/src/app/inspect/inspect.component.ts
+++ client/src/app/inspect/inspect.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NotFoundError } from 'rxjs';
 import { UserService } from 'src/user.service';
+import { ErrorHandler } from '@angular/core';
 
 
 interface UserData {
@@ -19,13 +20,15 @@ interface UserData {
   following: number;
 }
 
+let noUser = '';
+
 @Component({
   selector: 'app-inspect',
   templateUrl: './inspect.component.html',
   styleUrls: ['./inspect.component.css']
 })
 export class InspectComponent implements OnInit {
-  
+
   username: string = ""
   newUser: UserData = {
     username: "",
@@ -51,13 +54,19 @@ export class InspectComponent implements OnInit {
 
   receiveUsername(valueEmitted: string) {
     this.username = valueEmitted;
-    
   }
 
   onSubmit() {
+    try {
     this.userService.inspectUser(this.username)
       .then(result => { if (result) { this.newUser = result } })
-      .catch(NotFoundError => console.error("The user `${this.username}` was not found..."))
+    }
+      catch (error) {
+        if (this.newUser.username === "" || this.newUser.username === undefined) {
+          console.log(`The user ${this.username} was not found...`)
+        }
+      console.error('Error fetching user data:', error);
+    }
   }
 
 }
